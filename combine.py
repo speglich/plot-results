3636fe4c6b3f06f007edeabbca67d5856ef4d925
Gustavo Coelho <gustavo.coelho@fbter.org.br>
2022-11-07 11:46:14 -0300

    Update files to plot blocked time stepping results

--- combine.py
+++ combine.py
@@ -4,11 +4,17 @@ import pandas as pd
 
 extension = 'csv'
 
-results = {'naive' : 'naive-checkpoint',
-            'cupy' : 'cupy-checkpoint'}
+results = {'ckp2' : 'results/ckp_2',
+           'ckp4' : 'results/ckp_4',
+           'ckp8' : 'results/ckp_8',
+           'ckp16' : 'results/ckp_16',
+           'ckp32' : 'results/ckp_32',
+           'ckp64' : 'results/ckp_64',
+           'ckp128' : 'results/ckp_128'}
 
 for r in results:
     all_filenames = [i for i in glob.glob('{}/*.{}'.format(results[r], extension))]
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
     combined_csv = combined_csv[combined_csv.columns.drop(list(combined_csv.filter(regex='count')))]
+    combined_csv = combined_csv[combined_csv.columns.drop(list(combined_csv.filter(regex='N_CKP')))]
     combined_csv.to_csv('{}.csv'.format(r))
--- plot.py
+++ plot.py
@@ -5,39 +5,25 @@ from matplotlib import pyplot as plt
 # Arrumar titulo na posição Certa 
 # Colocar em segundos
 
+for nckp in [2, 4, 8, 16, 32, 64, 128]:
+    df_ckp = pd.read_csv(f'ckp{nckp}.csv')
+    df_ckp = df_ckp.sort_values(by='BLK')
+    df_ckp['experiment'] = df_ckp.apply(lambda row: "%d"%(row['BLK']), axis=1)
+    df_ckp = df_ckp.set_index('experiment')
+    df_ckp = df_ckp.filter(regex='timing')
+    df_ckp = df_ckp[df_ckp.columns.drop(list(df_ckp.filter(regex='storage')))]
+    df_ckp = df_ckp.div(1000).round(3)
 
-df_naive = pd.read_csv('naive.csv')
-df_naive = df_naive.sort_values(by='N_CKP')
-df_naive['experiment'] = df_naive.apply(lambda row: "DA - CKPs %d"%(row['N_CKP']), axis=1)
-df_naive = df_naive.set_index('experiment')
-df_naive = df_naive.filter(regex='timing')
-df_naive = df_naive.div(1000).round(3)
+    df = pd.concat([df_ckp], axis=0)
 
+    ax = df.plot(kind='bar',stacked=True)
 
-df_cupy = pd.read_csv('cupy.csv')
-df_cupy = df_cupy.sort_values(by='N_CKP')
-df_cupy['experiment'] = df_cupy.apply(lambda row: "UMA - CKPs %d"%(row['N_CKP']), axis=1)
-df_cupy = df_cupy.set_index('experiment')
-df_cupy = df_cupy.filter(regex='timing')
-df_cupy = df_cupy.div(1000).round(3)
+    title = f'Execution Time [s] per Block Size - {nckp} Checkpoints - CPU'
 
-df_external = pd.read_csv('external_allocator.csv')
-df_external = df_external.sort_values(by='N_CKP')
-df_external['experiment'] = df_external.apply(lambda row: "EA - CKPs %d"%(row['N_CKP']), axis=1)
-df_external = df_external.set_index('experiment')
-df_external = df_external.filter(regex='timing')
-df_external = df_external.div(1000).round(3)
-
-df = pd.concat([df_naive, df_cupy, df_external], axis=0)
-
-ax = df.plot(kind='bar',stacked=True)
-
-title = 'Execution Time Default Allocator (DA) x Unified Memory Allocator (UMA) x External Allocator (EA)'
-
-
-ax.set_ylabel('Time [s]')
-ax.set_xlabel('Experiments per Number of Checkpoints')
-ax.set_title(title)
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig('figures/results.png', dpi=350, bbox_inches='tight')
+    ax.set_xticklabels(df.index,rotation=0)
+    ax.set_ylabel('Time [s]')
+    ax.set_xlabel('Block Size')
+    ax.set_title(title)
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig(f'figures/blk-checkpoint{nckp}.png', dpi=350, bbox_inches='tight')
 # print(keys)
